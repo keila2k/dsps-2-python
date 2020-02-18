b4be3028f9ebc862e642423eeaed9b3419df034d
okeila <ranZiv93>
2020-02-18 14:43:09 +0200

    c0 mapper and reducer phsase 1

--- c0_mapper1.py
+++ c0_mapper1.py
@@ -12,8 +12,8 @@ def cleanWord(aword):
        interpreted as a single word.
     Output: a clean, lower-case version of the word
     """
-    regex = r"[a-zA-Z]+"
-    matches = re.match(regex, aword, re.MULTILINE)
+    regex = r"[a-zA-Z0-9_]+"
+    matches = bool(re.search(regex, aword, re.MULTILINE))
     if matches:
         return None
     return aword
@@ -24,14 +24,13 @@ def read_input():
         # split the line into words
         yield line.strip().split()
 
-
 def main(separator='\t'):
     # input comes from STDIN (standard input)
     data = read_input()
     for line in data:
         # write the results to STDOUT (standard output);
         # what we output here will be the input for the
-        # Reduce step, i.e. the input for reducer.py
+        # Reduce step, i.e. the input for c0_reducer1.py
         #
         word = cleanWord(line[0])
         if word is None:
